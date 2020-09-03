7164a019477384642b8fd97521ae89556918518e
SorinGabriel02 <lupu_sg@yahoo.com>
2020-09-03 21:32:36 +0300

    small fix

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -6,6 +6,10 @@ const Projects = () => {
   return (
     <main className={projectsContainer}>
       <h1>What I've Been Working On</h1>
+      <p>
+        This section of the portfolio is under construction and will be updated
+        soon...
+      </p>
       <section className={projectCard}>
         <h2>Support Teachers</h2>
       </section>
